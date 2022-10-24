84d15fb977f510f3e3a40914e752647158b51789
Ezequiel <incuttoezequiel858@gmail.com>
2022-10-24 20:24:17 -0300

    Desafio

---
+++ Desafio_clases_Incutto.js
@@ -0,0 +1,43 @@
+class usuario {
+    constructor(nombre, apellido, libros, mascota){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascota = mascota;
+    }
+    getFullnombre (){
+
+        return console.log(`Hola mi nombre es ${this.nombre} ${this.apellido}`)
+    }
+    addmascota (nombre_mascota){
+        this.mascota.push (nombre_mascota);
+    }
+    CountMascotas (){
+        return console.log(`La cantidad de mascotas que tiene es: ${this.mascota.length}`)
+    }
+    addLibro(){
+        this.libros.push({
+            Nombre: Nombrelibro,
+            Autor:  Autor,
+        });
+    }
+    getNombreslibros(){
+        return console.log(this.libros.map((libros) => libros.Nombre))
+    }
+}
+const Usuario1 = new usuario('Ezequiel', 'Incutto',[
+    {
+        Nombre: 'Harry Potter',
+        Autor: 'J. K. Rowling',
+    },
+    {
+        Nombre: 'El principito',
+        Autor: 'Antoine de Saint-Exupéry',
+    }],
+    ["Nicky", "Nieves"]);
+
+Usuario1.addmascota("Morita");
+Usuario1.addLibro(Nombrelibro = "El Señor de los Anillos", Autor = "J. R. R. Tolkien")
+Usuario1.getFullnombre()
+Usuario1.CountMascotas()
+Usuario1.getNombreslibros()
